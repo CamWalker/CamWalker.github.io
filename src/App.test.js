9007c730bdb13125ea486cb9c4ddb1f06c3bc9b9
Cameron Walker <whatiscameronsemail@gmail.com>
2026-01-19 22:24:27 -0700

    Commit all!

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('sample test', () => {
-  // render(<App />);
-	// const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-	expect(true).toBeTruthy();
-});
